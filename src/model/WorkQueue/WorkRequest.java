58286d613390dfc5b0f974f5a0e1a0945469d5e2
Richa Jain <jain.ric@northeastern.edu>
2022-12-06 13:19:26 -0500

    Added WorkQueue and WorkRequest

---
+++ src/model/WorkQueue/WorkRequest.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.WorkQueue;
+
+import java.util.Date;
+import model.User;
+
+/**
+ *
+ * @author richajain
+ */
+public abstract class WorkRequest {
+    
+    private String message;
+    private User sender;
+    private User receiver;
+    private String status;
+    private Date requestDate;
+    private Date resolveDate;
+    
+    public WorkRequest(){
+        requestDate = new Date();
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getRequestDate() {
+        return requestDate;
+    }
+
+    public void setRequestDate(Date requestDate) {
+        this.requestDate = requestDate;
+    }
+
+    public Date getResolveDate() {
+        return resolveDate;
+    }
+
+    public void setResolveDate(Date resolveDate) {
+        this.resolveDate = resolveDate;
+    }
+}
